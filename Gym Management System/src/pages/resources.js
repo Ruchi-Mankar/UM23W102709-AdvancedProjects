e01e584063a601d710b687f2e97baf42d04eeaed
Ruchi-Mankar <114324342+Ruchi-Mankar@users.noreply.github.com>
2023-11-15 21:47:32 +0530

    Create resources.js

---
+++ Gym Management System/src/pages/resources.js
@@ -0,0 +1,7 @@
+const resources = {
+    password : "EseQhScKvz6SyZi5PWFD0K13KltVdU1QycrJdvAN4gc7nXCBzn3sv1NWiXeReYy6" , 
+    url : "https://fitnessprogym.hasura.app/api/rest"
+}
+
+export default resources
+// Ruchi Mankar UM23W102709
