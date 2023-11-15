92485c286a533c4f16f5838d7732327439395948
Ruchi-Mankar <114324342+Ruchi-Mankar@users.noreply.github.com>
2023-11-15 20:04:01 +0530

    Advanced Project

---
+++ StudentTeacherBooking/src/Admin/DeleteComponent.js
@@ -0,0 +1,19 @@
+// By: Ruchi Mankar UM23W102709
+import React from "react";
+import { rb } from "../Firebase";
+import { remove, ref } from "firebase/database";
+import Button from 'react-bootstrap/Button';
+
+function DeleteComponent({ id, onHide }) {
+  const handleDelete = () => {
+    remove(ref(rb, `teachers/${id}`)).then(() => onHide());
+  };
+
+  return (
+    <Button variant="danger" onClick={handleDelete}>
+      Delete
+    </Button>
+  );
+}
+
+export default DeleteComponent;
